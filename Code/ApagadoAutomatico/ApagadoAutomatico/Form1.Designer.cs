e7e57f6657135dca9c85ee3f001a291987bb493b
Juan Cruz <juancruzbreppe@gmail.com>
2020-10-22 12:22:45 -0300

    Version 2.0.0
    Agrego el codigo

---
+++ Code/ApagadoAutomatico/ApagadoAutomatico/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ApagadoAutomatico
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnApagar = new System.Windows.Forms.Button();
+			this.btnCancelar = new System.Windows.Forms.Button();
+			this.dtpHoras = new System.Windows.Forms.DateTimePicker();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnApagar
+			// 
+			this.btnApagar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			resources.ApplyResources(this.btnApagar, "btnApagar");
+			this.btnApagar.Name = "btnApagar";
+			this.btnApagar.UseVisualStyleBackColor = false;
+			this.btnApagar.Click += new System.EventHandler(this.BtnApagar_Click);
+			// 
+			// btnCancelar
+			// 
+			this.btnCancelar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			resources.ApplyResources(this.btnCancelar, "btnCancelar");
+			this.btnCancelar.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.btnCancelar.Name = "btnCancelar";
+			this.btnCancelar.UseVisualStyleBackColor = false;
+			this.btnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+			// 
+			// dtpHoras
+			// 
+			resources.ApplyResources(this.dtpHoras, "dtpHoras");
+			this.dtpHoras.CalendarMonthBackground = System.Drawing.SystemColors.ActiveCaption;
+			this.dtpHoras.Name = "dtpHoras";
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label1.Name = "label1";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label2.Name = "label2";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dtpHoras);
+			this.Controls.Add(this.btnCancelar);
+			this.Controls.Add(this.btnApagar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnApagar;
+		private System.Windows.Forms.Button btnCancelar;
+		private System.Windows.Forms.DateTimePicker dtpHoras;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Code/ApagadoAutomatico/ApagadoAutomatico/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApagadoAutomatico
+{
+	public partial class Form1 : Form
+	{
+		string cadena;
+		int seg;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			dtpHoras.Format = DateTimePickerFormat.Custom;
+			dtpHoras.CustomFormat = "HH:mm";
+			dtpHoras.ShowUpDown = true;
+			reiniciar();
+			habilitar(true);
+		}
+
+		private void BtnApagar_Click(object sender, EventArgs e)
+		{
+			seg = (Convert.ToInt32(dtpHoras.Value.Hour) * 3600) + (Convert.ToInt32(dtpHoras.Value.Minute) * 60); //CALCULO PARA PASAR DE HH:MM A SEGUNDOS (LA CMD TOMA EL TIEMPO EN SEGUNDOS) - CALCULATION TO GO FROM HH: MM TO SECONDS (THE CMD TAKES THE TIME IN SECONDS)
+			if (seg<10 || seg> 315359999) //VALIDACION - VALIDATION
+			{
+				MessageBox.Show("Ese tiempo no se puede, inténtalo de nuevo!","ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+				reiniciar();
+				habilitar(true);
+			}
+			else //CODIGO EN CMD PARA APAGAR LA PC - CODE IN CMD FOR SHUTDOWN THE PC
+			{
+				cadena = "/c shutdown -s -t " + seg.ToString();
+				System.Diagnostics.Process.Start("CMD.exe", cadena);
+				reiniciar();
+				habilitar(false);
+			}
+			
+		}
+
+		private void BtnCancelar_Click(object sender, EventArgs e) //CODIGO PARA CANCELAR EL APAGADO - CODE FOR CANCEL THE SHUTDOWN
+		{
+			cadena = "/c shutdown -a";
+			System.Diagnostics.Process.Start("CMD.exe", cadena);
+			reiniciar();
+			habilitar(true);
+		}
+		public void reiniciar() //REINICIA EL CONTADOR A 00:00 - REBOOT THE COUNT TO 00:00
+		{
+			dtpHoras.Text = "00:00";
+		}
+		public void habilitar(bool x) //METODO PARA ACTIVAR Y DESACTIVAR BOTONES - METHOD FOR ACTIVATE AND DESACTIVATE THE BUTTONS
+		{
+			btnApagar.Enabled = x;
+			btnCancelar.Enabled = !x;
+		}
+	}
+}
